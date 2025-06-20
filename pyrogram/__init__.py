4b74e9461138cc1dee9776eee42dd503cf661bc8
MDzxbotz <126398155+MDzxbotz@users.noreply.github.com>
2025-06-21 03:40:54 +0530

    Fix

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -40,4 +40,4 @@ from .client import Client
 from .sync import idle, compose
 
 #crypto_executor = ThreadPoolExecutor(300, thread_name_prefix="CryptoWorker")
-crypto_executor = ProcessPoolExecutor(8, thread_name_prefix="CryptoWorker")
+crypto_executor = ProcessPoolExecutor(8)
