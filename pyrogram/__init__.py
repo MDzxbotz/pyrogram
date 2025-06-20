9d201fb39fba169170ed7a91e8699008510efa4a
MDzxbotz <126398155+MDzxbotz@users.noreply.github.com>
2025-06-20 18:10:34 +0530

    Increase thread

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -16,7 +16,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "2.0.107"
+__version__ = "2.0.108"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2017-present Dan <https://github.com/delivrance>"
 
@@ -39,4 +39,4 @@ from . import raw, types, filters, handlers, emoji, enums
 from .client import Client
 from .sync import idle, compose
 
-crypto_executor = ThreadPoolExecutor(32, thread_name_prefix="CryptoWorker")
+crypto_executor = ThreadPoolExecutor(300, thread_name_prefix="CryptoWorker")
