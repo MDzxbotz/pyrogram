51ca082c8e3d52287cd8aabebdbc5fffd359b694
MDzxbotz <126398155+MDzxbotz@users.noreply.github.com>
2025-06-21 03:38:34 +0530

    Fix

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -20,8 +20,8 @@ __version__ = "2.0.108"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2017-present Dan <https://github.com/delivrance>"
 
-from concurrent.futures.thread import ThreadPoolExecutor, ProcessPoolExecutor
-
+from concurrent.futures.thread import ThreadPoolExecutor
+from concurrent.futures.process import ProcessPoolExecutor
 
 class StopTransmission(Exception):
     pass
