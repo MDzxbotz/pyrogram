20eb6707584ccdeb8a73c062cc350dbd7625dc3f
MDzxbotz <126398155+MDzxbotz@users.noreply.github.com>
2025-06-21 22:30:58 +0530

    Connection (#1)

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -16,12 +16,12 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "2.0.106"
+__version__ = "2.0.108"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2017-present Dan <https://github.com/delivrance>"
 
 from concurrent.futures.thread import ThreadPoolExecutor
-
+from concurrent.futures.process import ProcessPoolExecutor
 
 class StopTransmission(Exception):
     pass
@@ -39,4 +39,5 @@ from . import raw, types, filters, handlers, emoji, enums
 from .client import Client
 from .sync import idle, compose
 
-crypto_executor = ThreadPoolExecutor(5, thread_name_prefix="CryptoWorker")
+#crypto_executor = ThreadPoolExecutor(300, thread_name_prefix="CryptoWorker")
+crypto_executor = ProcessPoolExecutor(5)
